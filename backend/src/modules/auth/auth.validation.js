ba091e3721d86846e7c803172de6782c323b6ea4
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-05-11 12:07:55 +0300

    Validation schema for Auth.

---
+++ backend/src/modules/auth/auth.validation.js
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const authSchema = z
+  .object({
+    nameSurname: z.string(),
+    email: z.string().email(),
+    phoneNumber: z.string().optional(),
+    password: z.string().min(8),
+    passwordConfirm: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Passwords don't match.",
+    path: ['passwordConfirm'],
+  });
+
+export default {
+  authSchema,
+};
